b503ecfa1c5a5d6779649a937f22a76f4632cdb9
Anna Korzhenko <akorzhenko@kse.org.ua>
2024-06-09 11:34:14 +0300

    push

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <strings.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <conio.h>
 
+using namespace std;
+
+
+int main() {
+    cout << "Hello";
+    return 0;
+}
